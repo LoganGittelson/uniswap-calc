06110f8f36fe09ba730e48d8e86453e85103782c
LoganGittelson <logan.gittelson@gmail.com>
2022-07-26 00:03:29 -0400

    Checking values against given example

--- go.mod
+++ go.mod
@@ -2,11 +2,6 @@ module github.com/LoganGittelson/uniswap-calc
 
 go 1.18
 
-require github.com/machinebox/graphql v0.2.2
+require github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29
 
-require (
-	github.com/matryer/is v1.4.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29 // indirect
-	golang.org/x/net v0.0.0-20220725212005-46097bf591d3 // indirect
-)
+require golang.org/x/net v0.0.0-20220725212005-46097bf591d3 // indirect
--- main.go
+++ main.go
@@ -18,33 +18,42 @@ func main() {
 			Date    graphql.Int
 			TvlUSD  graphql.String `graphql:"tvlUSD"`
 			FeesUSD graphql.String `graphql:"feesUSD"`
-		} `graphql:"poolDayDatas(first: 100, where: {date: $day, id_gt: $lastID})"`
+		} `graphql:"poolDayDatas(first: 1, where: {date: $day, id_gt: $lastID})"`
 	}
 
 	variables := map[string]interface{}{
 		"day":    graphql.Int(1647388800),
-		"lastID": "",
+		"lastID": "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d7-19067",
 	}
 
-	err := client.Query(context.Background(), &q, variables)
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
 
-	for _, p := range q.PoolDayDatas {
-		log.Printf(
-			`
+		err := client.Query(context.Background(), &q, variables)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, p := range q.PoolDayDatas {
+			log.Printf(
+				`
 			ID: %v
 			Date: %v
 			TVL: %v
 			Fees: %v
 			`, p.Id, p.Date, p.TvlUSD, p.FeesUSD,
-		)
+			)
+		}
+
+		log.Printf("Records retrieved: %d", len(q.PoolDayDatas))
+
+		if len(q.PoolDayDatas) == 0 {
+			break
+		}
+		break
+
+		// TODO: Add pagination
+		// TODO: Iterate through dates
+		// TODO: Store to cummlative sums
 	}
-
-	log.Printf("Records retrieved: %d", len(q.PoolDayDatas))
-
-	// TODO: Add pagination
-	// TODO: Store to cummlative sums
 
 }
